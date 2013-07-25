79761b77c5426010890e0c0fb0e6815409188a99
Malte Toenjes <malte@toenjes.eu>
2013-07-25 13:07:01 +0200

    added column username to Database and formlayout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
-  validates_presence_of :password, :on => :create
+  validates_presence_of :password,:email, :name, :on => :create
 end
