dd34aed530a42ce39f6e2e55b89d6661ab4dc5f3
Malte Toenjes <malte@toenjes.eu>
2013-07-26 10:55:09 +0200

    template hinzugefügt, tabelle album, tabelle images

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,30 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130725082707) do
+ActiveRecord::Schema.define(:version => 20130726083249) do
+
+  create_table "albums", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "images", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "path"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+    t.string   "name",            :limit => 20
+    t.string   "avatar"
   end
 
 end
